03ab40930a356b2d2199219dd5077fc37cebc664
blackundo <blackundovn@gmail.com>
2022-11-11 00:51:21 +0700

    add DanhGiaDAO :rocket:

---
+++ src/main/java/com/undotech/dao/DanhGiaDAO.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.undotech.dao;
+
+import com.undotech.entity.DanhGia;
+import java.util.List;
+
+/**
+ *
+ * @author blackundo
+ */
+public class DanhGiaDAO extends QLyKhachSanDAO<DanhGia, Integer> {
+
+    @Override
+    public void insert(DanhGia entity) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void update(DanhGia entity) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void delete(Integer key) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<DanhGia> selectAll() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public DanhGia selectById(Integer key) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    protected List<DanhGia> selectBySql(String sql, Object... args) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
--- src/main/java/com/undotech/utils/MsgBox.java
+++ src/main/java/com/undotech/utils/MsgBox.java
@@ -13,15 +13,15 @@ import javax.swing.JOptionPane;
  */
 public class MsgBox {
     public static void alert(Component parent, String message) {
-        JOptionPane.showMessageDialog(parent, message, "EduSys", JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(parent, message, "UntoTech", JOptionPane.INFORMATION_MESSAGE);
     }
 
     public static boolean confirm(Component parent, String message) {
-        int result = JOptionPane.showConfirmDialog(parent, message, "EduSys", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+        int result = JOptionPane.showConfirmDialog(parent, message, "UntoTech", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
         return result == JOptionPane.YES_OPTION;
     }
 
     public static String prompt(Component parent, String message) {
-        return JOptionPane.showInputDialog(parent, message, "EduSys", JOptionPane.INFORMATION_MESSAGE);
+        return JOptionPane.showInputDialog(parent, message, "UntoTech", JOptionPane.INFORMATION_MESSAGE);
     }
 }
