6473f8b33ffe5af3b3ad3123bc78ac8c13cefd8d
blackundo <blackundovn@gmail.com>
2022-11-11 01:18:50 +0700

    add Auth.java  :fire::fire:

---
+++ src/main/java/com/undotech/utils/Auth.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.undotech.utils;
+
+import com.undotech.entity.NhanVien;
+
+/**
+ *
+ * @author blackundo
+ */
+public class Auth {
+    public static NhanVien user = null;
+    public static void clear(){
+        Auth.user = null;
+    }
+    public static boolean isLogin(){
+        return Auth.user != null;
+    }
+    public static String role(){
+        String role = null;
+        if (Auth.isLogin()) {
+            role = user.getChucVu();
+        }
+        return role;
+    }
+}
