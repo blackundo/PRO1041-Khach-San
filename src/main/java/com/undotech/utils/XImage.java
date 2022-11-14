bb97a780c9bd2b1087ef9608cb7b1f5c6c34d78d
blackundo <blackundovn@gmail.com>
2022-11-15 00:16:01 +0700

    :fire: add utils/XImage  :rocket:

---
+++ src/main/java/com/undotech/utils/XImage.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.undotech.utils;
+
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author blackundo
+ */
+public class XImage {
+    public static void save(File src) {
+        File dir = new File("logos", src.getName());
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        try {
+            Path source = Paths.get(src.getAbsolutePath());
+            Path destination = Paths.get(dir.getAbsolutePath());
+            Files.copy(source, destination, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public static ImageIcon read(String filename) {
+        File path = new File("logos", filename);
+        return new ImageIcon(new ImageIcon(path.getAbsolutePath()).getImage().getScaledInstance(180, 180, Image.SCALE_DEFAULT));
+    }
+}
